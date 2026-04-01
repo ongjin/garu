60b1adb3980d66affa0dd83cb123ea3faf677409
조용진 <dydwls140@naver.com>
2026-04-01 16:50:19 +0900

    chore: bump to 0.4.1

--- js/pkg/garu_wasm.js
+++ js/pkg/garu_wasm.js
@@ -94,7 +94,7 @@ export class GaruWasm {
     }
 }
 if (Symbol.dispose) GaruWasm.prototype[Symbol.dispose] = GaruWasm.prototype.free;
-import * as import1 from "./snippets/garu-core-2da6ed4f058f2483/inline0.js"
+import * as import1 from "./snippets/garu-core-dc89881188f44ead/inline0.js"
 
 function __wbg_get_imports() {
     const import0 = {
@@ -155,7 +155,7 @@ function __wbg_get_imports() {
     return {
         __proto__: null,
         "./garu_wasm_bg.js": import0,
-        "./snippets/garu-core-2da6ed4f058f2483/inline0.js": import1,
+        "./snippets/garu-core-dc89881188f44ead/inline0.js": import1,
     };
 }
 
